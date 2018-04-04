e111399a97910d3dc0b58aa241774eb2131c028a
Amirul Zharfan bin Zalid <am.zharfan@gmail.com>
2018-04-04 18:55:18 +0800

    Fix something

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
+var todo = document.getElementById("todo");
+var unauthorized = document.getElementById("unauthorized");
+var headerTitle = document.querySelector('.header h1');
+
 var name = prompt("Enter your name: ");
 var password = prompt("Enter your password: ");
 
-var todo = document.getElementById("todo");
-var unauthorized = document.getElementById("unauthorized");
-
 if(password == "123") {
   alert("Welcome! " + name);
-  todo.style.display = "block";
+  todo.style.display = "flex";
   unauthorized.style.display = "none";
+  headerTitle.innerHTML = "Hi " + name + "!";
 } else {
   alert("Unauthorized access!");
   todo.style.display = "none";
-  unauthorized.style.display = "block";
+  unauthorized.style.display = "flex";
 }
 
 function addTodo(e) {
